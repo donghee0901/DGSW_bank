0642552cfa83680e631f3f108de9f174aa26aeb2
KimDongHee <donghee0901@naver.com>
2020-06-23 15:18:33 +0900

    Make AccountManager and Add Copy_Constructor
    + retouch Console print

--- DGSW_bank/DGSW_bank/Bank_main.cpp
+++ DGSW_bank/DGSW_bank/Bank_main.cpp
@@ -5,56 +5,84 @@
 #define MAX_ACCOUNT 100
 using namespace std;
 
+
 class Account
 {
 public:
 	//계좌 개설(생성자)
-	Account(int accID, int balance, char* cusName, char* socialSecurityNumber)
+	Account(int accID, int balance, char* cusName, char* socialSecurityNumber) :accID(accID), socialSecurityNumber(socialSecurityNumber)
 	{
-		this->accID = accID;
 		this->balance = balance;
 		this->cusName = cusName;
-		this->socialSecurityNumber = socialSecurityNumber;
 	}
 
+	//계좌 복사(복사생성자)
+	Account(const Account &copy_member) :accID(copy_member.accID)
+	{
+		cout << "Copy_Constructor" << endl;
+		this->balance = copy_member.balance;
+		char *name = new char[NAME_LEN];
+		strcpy_s(name, NAME_LEN, copy_member.cusName);
+		this->cusName = name;
+		char *social = new char[SOCIALSECURITYNUMBER_LEN];
+		strcpy_s(social, SOCIALSECURITYNUMBER_LEN, copy_member.socialSecurityNumber);
+		this->socialSecurityNumber = social;
+	}
+
+	//소멸자
 	~Account()
 	{
 		if (cusName != nullptr) delete cusName;
 		if (socialSecurityNumber != nullptr) delete socialSecurityNumber;
 	}
 
+	//콘솔 지우기
+	void Console_clear()
+	{
+		system("cls");
+		cout << endl;
+	}
+
+	//입금(클래스 내 메소드)
 	void Deposit(int deposit)
 	{
 		if (deposit <= 0) {
+			Console_clear();
 			cout << "입금액은 최소 1원 이상이여야 합니다." << endl;
 			return;
 		}
 
 		balance += deposit;
+		Console_clear();
 		cout << endl << cusName << " 고객님의 계좌에 " << deposit << "원이 입금되었습니다." << endl;
 		cout << "잔액 : " << balance << "원" << endl;
 		return;
 	}
-	
+
+	//출금(클래스 내 메소드)
 	void Withdraw(int withdraw)
 	{
 		if (balance < withdraw) {
+			Console_clear();
 			cout << "잔액이 부족합니다." << endl;
 			cout << "잔액 : " << balance << "원" << endl;
 			return;
 		}
 
 		if (withdraw <= 0) {
+			Console_clear();
 			cout << "출금액은 최소 1원 이상이여야 합니다." << endl;
 			return;
 		}
 
 		balance -= withdraw;
+		Console_clear();
 		cout << endl << cusName << " 고객님의 계좌에서 " << withdraw << "원이 출금되었습니다." << endl;
 		cout << "잔액 : " << balance << "원" << endl;
 		return;
 	}
 
+	//계좌정보 출력(클래스 내 메소드)
 	void Print_All()
 	{
 		cout << endl;
@@ -64,6 +92,7 @@ public:
 		cout << "주민번호: " << socialSecurityNumber << endl;
 	}
 
+	//get, set함수(클래스 내 메소드)
 	int getaccID()
 	{
 		return accID;
@@ -76,35 +105,198 @@ public:
 	{
 		return cusName;
 	}
-	char* getsocialSecurityNumber()
+	const char* getsocialSecurityNumber()
 	{
 		return socialSecurityNumber;
 	}
 private:
-	int accID;
+	const int accID;
 	int balance;
-	char *cusName;
-	char *socialSecurityNumber;
+	char *cusName = nullptr;
+	const char *socialSecurityNumber = nullptr;
 };
+class AccountManager
+{
+public:
+	~AccountManager()
+	{
+		for (int i = 0; i < MAX_ACCOUNT; i++) {
+			if (member[i] != nullptr) delete member[i];
+		}
+	}
 
-Account *member[MAX_ACCOUNT];
-int member_count = 0;
+	//콘솔 지우기
+	void Console_clear()
+	{
+		system("cls");
+		cout << endl;
+	}
 
-bool Have_Account();
-int Select_AccID(int accID);
-int Select_SocialSecurityNumber(char* socialSecurityNumber);
+	//계좌 여부 확인
+	bool Have_Account()
+	{
+		if (member_count == 0) {
+			Console_clear();
+			cout << "현재 등록된 계좌가 없습니다." << endl;
+			return false;
+		}
+		return true;
+	}
+
+	//accID 중복 확인
+	int Select_AccID(int accID)
+	{
+		for (int i = 0; i < member_count; i++) {
+			if (member[i]->getaccID() == accID) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	//주민번호 중복 확인
+	int Select_SocialSecurityNumber(char* socialSecurityNumber)
+	{
+		for (int i = 0; i < member_count; i++) {
+			if (strcmp(socialSecurityNumber, member[i]->getsocialSecurityNumber()) == 0) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	//계좌 개설
+	void Make_Account()
+	{
+		int select_ID;
+		int select_SocialSecurityNumber;
+
+		int accID;
+		int balance;
+		char *cusName = new char[NAME_LEN];
+		char *socialSecurityNumber = new char[SOCIALSECURITYNUMBER_LEN];
+
+		if (member_count >= MAX_ACCOUNT) {
+			Console_clear();
+			cout << "은행에 가입된 사람이 모두 찼습니다." << endl;
+			return;
+		}
+		Console_clear();
+		cout << "[계좌개설]" << endl;
+		cout << "계좌ID: ";
+		cin >> accID;
+
+		select_ID = Select_AccID(accID);
+		if (select_ID >= 0) {
+			Console_clear();
+			cout << "계좌번호가 중복됩니다." << endl;
+			return;
+		}
+
+		cout << "이  름: ";
+		cin >> cusName;
+
+		cout << "입금액: ";
+		cin >> balance;
+		if (balance < 10) {
+			Console_clear();
+			cout << "계좌개설 시 최소 10원 이상의 입금액이 필요합니다." << endl;
+			return;
+		}
+
+		cout << "주민번호: ";
+		cin >> socialSecurityNumber;
+
+		select_SocialSecurityNumber = Select_SocialSecurityNumber(socialSecurityNumber);
+		if (select_SocialSecurityNumber != -1) {
+			Console_clear();
+			cout << "주민번호가 중복됩니다." << endl;
+			return;
+		}
+
+		member[member_count] = new Account(accID, balance, cusName, socialSecurityNumber);
+
+		member_count++;
+	}
+
+	//입    금
+	void Deposit()
+	{
+		if (Have_Account() == false) {
+			return;
+		}
+		int accID;
+		int deposit;
+		int select_ID;
+
+		Console_clear();
+		cout << "[입    금]" << endl;
+		cout << "계좌ID: ";
+		cin >> accID;
+
+		select_ID = Select_AccID(accID);
+		if (select_ID == -1) {
+			Console_clear();
+			cout << "유효하지 않은 ID입니다." << endl;
+			return;
+		}
+		cout << "입금액: ";
+		cin >> deposit;
+
+		member[select_ID]->Deposit(deposit);
+	}
+
+	//출    금
+	void Withdraw()
+	{
+		if (Have_Account() == false) {
+			return;
+		}
+		int accID;
+		int withdraw;
+		int select_ID;
+
+		Console_clear();
+		cout << "[출    금]" << endl;
+		cout << "계좌ID: ";
+		cin >> accID;
+
+		select_ID = Select_AccID(accID);
+		if (select_ID == -1) {
+			Console_clear();
+			cout << "유효하지 않은 ID입니다." << endl;
+			return;
+		}
+		cout << "출금액: ";
+		cin >> withdraw;
+
+		member[select_ID]->Withdraw(withdraw);
+	}
+
+	//계좌정보 전체 출력
+	void Print_All()
+	{
+		Console_clear();
+		cout << "[계좌정보 전체 출력]" << endl;
+		if (Have_Account() == false) {
+			return;
+		}
+		for (int i = 0; i < member_count; i++) {
+			member[i]->Print_All();
+		}
+	}
+private:
+	Account *member[MAX_ACCOUNT];
+	int member_count = 0;
+};
+AccountManager manager;
+
 void Select();
-void Make_Account();
-void Deposit();
-void Withdraw();
-void Print_All();
 
-void main()
+int main()
 {
 	//시작하자마자 메뉴 선택창으로 감
 	Select();
 }
-
 //메뉴 선택
 void Select()
 {
@@ -126,163 +318,21 @@ void Select()
 		switch (select)
 		{
 		case 1:
-			Make_Account();
+			manager.Make_Account();
 			break;
 		case 2:
-			Deposit();
+			manager.Deposit();
 			break;
 		case 3:
-			Withdraw();
+			manager.Withdraw();
 			break;
 		case 4:
-			Print_All();
+			manager.Print_All();
 			break;
 		case 5:
-			if(member != NULL) delete[] member;
 			return;
 		}
 		cout << endl << endl;
 	}
 }
 
-//계좌 여부 확인
-bool Have_Account()
-{
-	if (member_count == 0) {
-		cout << "현재 등록된 계좌가 없습니다." << endl;
-		return false;
-	}
-	return true;
-}
-
-//accID 중복 확인
-int Select_AccID(int accID)
-{
-	for (int i = 0; i < member_count; i++) {
-		if (member[i]->getaccID() == accID) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-//주민번호 중복 확인
-int Select_SocialSecurityNumber(char* socialSecurityNumber)
-{
-	for (int i = 0; i < member_count; i++) {
-		if (strcmp(socialSecurityNumber, member[i]->getsocialSecurityNumber()) == 0) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-//계좌 개설
-void Make_Account()
-{
-	int select_ID;
-	int select_SocialSecurityNumber;
-
-	int accID;
-	int balance;
-	char *cusName = new char[NAME_LEN];
-	char *socialSecurityNumber = new char[SOCIALSECURITYNUMBER_LEN];
-
-	if (member_count >= MAX_ACCOUNT) {
-		cout << "은행에 가입된 사람이 모두 찼습니다." << endl;
-		return;
-	}
-	cout << "[계좌개설]" << endl;
-	cout << "계좌ID: ";
-	cin >> accID;
-
-	select_ID = Select_AccID(accID);
-	if (select_ID >= 0) {
-		cout << "계좌번호가 중복됩니다." << endl;
-		return;
-	}
-
-	cout << "이  름: ";
-	cin >> cusName;
-
-	cout << "입금액: ";
-	cin >> balance;
-	if (balance < 10) {
-		cout << "계좌개설 시 최소 10원 이상의 입금액이 필요합니다." << endl;
-		return;
-	}
-
-	cout << "주민번호: ";
-	cin >> socialSecurityNumber;
-	
-	select_SocialSecurityNumber = Select_SocialSecurityNumber(socialSecurityNumber);
-	if (select_SocialSecurityNumber != -1) {
-		cout << "주민번호가 중복됩니다." << endl;
-		return;
-	}
-
-	member[member_count] = new Account(accID, balance, cusName, socialSecurityNumber);
-
-	member_count++;
-}
-
-//입    금
-void Deposit()
-{
-	if (Have_Account() == false) {
-		return;
-	}
-	int accID;
-	int deposit;
-	int select_ID;
-
-	cout << "[입    금]" << endl;
-	cout << "계좌ID: ";
-	cin >> accID;
-
-	select_ID = Select_AccID(accID);
-	if (select_ID == -1) {
-		cout << "유효하지 않은 ID입니다." << endl;
-		return;
-	}
-	cout << "입금액: ";
-	cin >> deposit;
-
-	member[select_ID]->Deposit(deposit);
-}
-
-//출    금
-void Withdraw()
-{
-	if (Have_Account() == false) {
-		return;
-	}
-	int accID;
-	int withdraw;
-	int select_ID;
-
-	cout << "[출    금]" << endl;
-	cout << "계좌ID: ";
-	cin >> accID;
-
-	select_ID = Select_AccID(accID);
-	if (select_ID == -1) {
-		cout << "유효하지 않은 ID입니다." << endl;
-		return;
-	}
-	cout << "출금액: ";
-	cin >> withdraw;
-
-	member[select_ID]->Withdraw(withdraw);
-}
-
-//계좌정보 전체 출력
-void Print_All()
-{
-	if (Have_Account() == false) {
-		return;
-	}
-	for (int i = 0; i < member_count; i++) {
-		member[i]->Print_All();
-	}
-}
